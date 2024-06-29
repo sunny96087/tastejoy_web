f0785612e505fe87c5b9b1340ceaf7173daa70d8
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-29 22:34:38 +0800

    feat: vue3 + vite 環境建立、套件引入

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div class="about">
+      <h1 class="text-3xl text-red-500">This is an about page</h1>
+    </div>
+  </main>
+</template>
