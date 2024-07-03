5d175a0969cbd30119c7329818ddddd26e65210a
紀博穎 <jiboying@MacBook-Pro.local>
2024-07-03 21:21:14 +0800

    docs: component-NavBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      backgroundColor: {
+        'custom-color': 'rgb(255, 253, 246)',
+      },
+      colors: {
+        'custom-font-color': '#6F6D55',
+      },
+    }
   },
   plugins: []
 }
