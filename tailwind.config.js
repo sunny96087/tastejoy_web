4cca14ffb5ecac2546152019bf8d63e8df9b7da5
紀博穎 <jiboying@MacBook-Pro.local>
2024-07-05 15:34:27 +0800

    feat: navbar rwd

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     extend: {
       backgroundColor: {
         'custom-color': 'rgb(255, 253, 246)',
+        'custom-hover-color':'rgb(235,229,186)',
+        'custom-alert-color':'rgb(255,174,170)'
       },
       colors: {
         'custom-font-color': '#6F6D55',
