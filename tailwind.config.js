26696c7175c789458351da2e75f72daadc90db4e
紀博穎 <jiboying@MacBook-Pro.local>
2024-07-09 11:49:42 +0800

    feat: foodRecordList

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ export default {
       backgroundColor: {
         'custom-color': 'rgb(255, 253, 246)',
         'custom-hover-color': 'rgb(235,229,186)',
-        'custom-alert-color': 'rgb(255,174,170)'
+        'custom-alert-color': 'rgb(255,174,170)',
+        'custom-switch-color':'rgb(247,246,241)',
       },
       colors: {
         'custom-font-color': '#6F6D55',
+        'custom-switch-color': 'rgb(164,157,125)',
         green1: '#EBE5BA',
         green2: 'rgba(189, 184, 144, 1)',
         green3: '#87835D',
