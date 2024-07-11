93d5f9a343eb51a36ae368498f7b41c4e265ab15
Ran <konankid10129@gmail.com>
2024-07-11 22:17:30 +0800

    text colour variables, font-size/ weight details and space corrected

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,10 +10,10 @@
       </div>
       <div class="w-[1280px] h-[360px] absolute inset-0 bg-black opacity-35"></div>
     </div>
-    <div class="absolute left-10 top-24 text-[#EBE5BA] space-y-4">
-      <h1 class="text-4xl font-bold">加入食享</h1>
-      <h2 class="text-2xl">一起來探索你的美食足跡，發現世界各地的美味佳餚</h2>
-      <h3 class="text-lg w-3/6">
+    <div class="absolute left-10 top-24 text-green1 space-y-4">
+      <h1 class="text-[28px] font-semibold">加入食享</h1>
+      <h2 class="text-xl font-semibold">一起來探索你的美食足跡，發現世界各地的美味佳餚</h2>
+      <h3 class="text-xl font-medium w-3/6">
         食享 (TasteJoy) 是一款專為美食愛好者設計的網站，
         讓您可以在全球範圍內探索、發現並記錄各種美食的足跡。
         無論您身處何地，我們都將帶您走進本地的美食天堂。
@@ -22,36 +22,36 @@
   </div>
 
   <div class="absolute left-10 top-[500px] bg-white text-center">
-    <h3 class="text-xl font-semibold text-[#6F6D55] mt-2 mb-2">功能介紹</h3>
+    <h3 class="text-xl font-semibold text-green4 mt-4 mb-1">功能介紹</h3>
     <div class="flex justify-center space-x-6">
       <div class="w-1/5 aspect-w-1 aspect-h-1 max-w-[220px] max-h-[220px]">
         <a href="#records">
           <img src="../assets/images/pic.jpg" alt="功能圖片1" class="w-full h-full object-cover rounded-lg cursor-pointer">
-          <p class="text-xs text-[#6F6D55] mt-2">紀錄自己的美食地圖</p>
+          <p class="text-xs text-green4 font-medium mt-2">紀錄自己的美食地圖</p>
         </a>
       </div>
       <div class="w-1/5 aspect-w-1 aspect-h-1 max-w-[220px]">
         <a href="#categories">
           <img src="../assets/images/pic.jpg" alt="功能圖片2" class="w-full h-full object-cover rounded-lg cursor-pointer">
-          <p class="text-xs text-[#6F6D55] mt-2">自訂美食分類</p>
+          <p class="text-xs text-green4 font-medium mt-2">自訂美食分類</p>
         </a>
       </div>
       <div class="w-1/5 aspect-w-1 aspect-h-1 max-w-[220px]">
         <a href="#randomSelected">
           <img src="../assets/images/pic.jpg" alt="功能圖片3" class="w-full h-full object-cover rounded-lg cursor-pointer">
-          <p class="text-xs text-[#6F6D55] mt-2">隨機選美食</p>
+          <p class="text-xs text-green4 font-medium mt-2">隨機選美食</p>
         </a>
       </div>
       <div class="w-1/5 aspect-w-1 aspect-h-1 max-w-[220px]">
         <a href="#articlesSharing">
           <img src="../assets/images/pic.jpg" alt="功能圖片4" class="w-full h-full object-cover rounded-lg cursor-pointer">
-          <p class="text-xs text-[#6F6D55] mt-2">美食文章分享</p>
+          <p class="text-xs text-green4 font-medium mt-2">美食文章分享</p>
         </a>
       </div>
       <div class="w-1/5 aspect-w-1 aspect-h-1 max-w-[220px]">
         <a href="#shareWithFriends">
           <img src="../assets/images/pic.jpg" alt="功能圖片5" class="w-full h-full object-cover rounded-lg cursor-pointer">
-          <p class="text-xs text-[#6F6D55] mt-2">和朋友分享美食紀錄</p>
+          <p class="text-xs text-green4 font-medium mt-2">和朋友分享美食紀錄</p>
         </a>
       </div>
     </div>
