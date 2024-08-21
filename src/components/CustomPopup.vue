bbac71631e7f840a4d03cba310bf4e3f669f2a0f
紀博穎 <jiboying@MacBook-Pro.local>
2024-08-21 15:48:50 +0800

    feat-foodRecordMap info icon

---
+++ src/components/CustomPopup.vue
@@ -0,0 +1,69 @@
+<script setup>
+
+const props = defineProps({
+    infoData:{
+        type: Object,
+        required: true,
+        default: () => ({
+            store: '',
+            category: '',
+            address: '',
+            businessHour: '',
+            phone: '',
+            score: '',
+        })
+    }
+    
+});
+
+</script>
+
+<template>
+    <div class="w-full bg-custom-color text-[#BDB890] rounded-lg shadow-lg overflow-auto">
+        <table class="w-full border-collapse text-center text-sm">
+        <thead>
+            <tr>
+            <th class="border-b px-4 py-2">名稱</th>
+            <th class="border-b px-4 py-2">分類</th>
+            <th class="border-b px-4 py-2">地點</th>
+            <th class="border-b px-4 py-2">營業時間</th>
+            <th class="border-b px-4 py-2">電話</th>
+            <th class="border-b px-4 py-2">分數</th>
+            <th class="border-b px-4 py-2">檢視</th>
+            <th class="border-b px-4 py-2">導航</th>
+            <th class="border-b px-4 py-2">分享</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td class="border-t px-4 py-2">{{ props.infoData.store }}</td>
+            <td class="border-t px-4 py-2">{{ props.infoData.category }}</td>
+            <td class="border-t px-4 py-2">{{ props.infoData.address }}</td>
+            <td class="border-t px-4 py-2">{{ props.infoData.businessHour }}</td>
+            <td class="border-t px-4 py-2">{{ props.infoData.phone }}</td>
+            <td class="border-t px-4 py-2">{{ props.infoData.score }}</td>
+            <td class="border-t px-4 py-2">
+                <button @click="showInfoRecord(record)">
+                    <img src="../assets/images/FoodRecordList/checkButton.png" alt="check-icon" style="width:28px; height:28px;">   
+                </button>    
+            </td>
+            <td class="border-t px-4 py-2">
+                <button>
+                    
+                    <img src="../assets/images/FoodRecordList/NavButton.png" alt="nav-icon" style="width:28px; height:28px;">
+                </button>
+            </td>
+            <td class="border-t px-4 py-2">
+                <button>
+                     
+                    <img src="../assets/images/FoodRecordList/ShareButton.png" alt="share-icon" style="width:28px; height:28px;">
+                </button>
+            </td>
+            </tr>
+        </tbody>
+        </table>
+    </div>    
+</template>
+
+<style scoped>
+</style>
--- src/views/FoodMapView.vue
+++ src/views/FoodMapView.vue
@@ -1,6 +1,45 @@
 <script setup>
-import { onMounted } from 'vue';
-import L from 'leaflet';
+import { onMounted, ref, createApp, h } from 'vue'
+import {  } from 'vue';
+import L from 'leaflet'
+import CustomPopup from '../components/CustomPopup.vue';
+import { useFoodRecordStore } from '../stores/foodRecord.js'
+
+// 取得美食紀錄資料 TODO:TOKEN API
+let isNoData = false;
+const token = ref('')
+const records = useFoodRecordStore().getFoodRecords(token)
+if(records.value.length === 0){
+  isNoData = true;
+}
+
+// 定義默認 marker 和selected-marker 的圖標
+const defaultIcon = L.icon({
+  iconUrl: 'src/assets/images/FoodRecordMap/location-marker-unselected.svg',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+});
+
+const selectedIcon = L.icon({
+  iconUrl: 'src/assets/images/FoodRecordMap/location-marker-selected.svg',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34]
+});
+
+// 記錄選中的 marker
+const selectedMarker = ref(null);
+
+// 重置所有 marker 的顏色為默認
+function resetMarkers(map) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.setIcon(defaultIcon);
+    }
+  });
+}
+
 
 onMounted(() => {
       // 獲取用户當前位置
@@ -18,6 +57,59 @@ onMounted(() => {
           L.marker([latitude, longitude]).addTo(map)
             .bindPopup('現在位置!')
             .openPopup();
+
+          // 添加用户儲存的紀錄位置
+          if(!isNoData){
+            // 處理紀錄陣列中的資料
+            for (var i = 0 ; i < records.value.length; i++) {
+                let location = records.value[i].location; //120.30,22.66
+                let itemLatitude = location.split(",")[0]; // 緯度
+                let itemLongitude = location.split(",")[1]; // 經度
+
+                //將紀錄mark起來
+                const marker = L.marker([Number(itemLongitude), Number(itemLatitude)], { icon: defaultIcon }).addTo(map);
+                const popupData = {
+                    store:records.value[i].store,
+                    category:records.value[i].category,
+                    address:records.value[i].address,
+                    businessHour:records.value[i].businessHour,
+                    phone:records.value[i].phone,
+                    score:records.value[i].score,
+                  };
+
+        
+                  let currentPopupMarker = null;
+                  marker.on('click', () => {
+                    
+                    // 設置當前的 marker 為新點選的 marker
+                    currentPopupMarker = marker;
+                    // 重置所有 marker 的顏色
+                    resetMarkers(map);
+
+                    // 設置當前選中的 marker
+                    marker.setIcon(selectedIcon);
+                    selectedMarker.value = marker;
+
+                    const container = document.createElement('div');    
+                    container.classList.add('custom-popup');  // 添加自定義樣式   
+                    
+                    const app = createApp({
+                      render() {
+                        return h(CustomPopup,{ infoData: popupData });
+                      }
+                    });
+
+                    app.mount(container);
+                    
+                    // 將彈出框直接插入到最外層的 #map 容器中
+                    const appContainer = document.getElementById('map');
+                    appContainer.appendChild(container);
+                 
+                });
+              }
+             
+          }
+           
         }, error => {
           console.error(error);
           alert('無法定位到您現在的位置！！');
@@ -27,12 +119,27 @@ onMounted(() => {
       }
     });
 
+
+
 </script>
 
 <template>
   <div id="map" style="height: 100vh;"></div>
-
 </template>
-<style scoped>
 
+<style>
+.custom-popup {
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  z-index: 1000;
+  width: 100%;
+  margin:0 auto;
+  height: auto;
+  background-color: #FFFDF6;
+  opacity: 0.9;
+  padding: 5px;
+  border: 2px solid #BDB890;
+  border-radius:0.75rem;
+}
 </style>
